e17cc00cf5e041d42afa02a6ddccaa4b621cb8ff
MTariq99 <you@example.com>
2022-05-06 15:52:43 +0500

    login function created

--- project/main.cpp
+++ project/main.cpp
@@ -3,24 +3,45 @@
 #include <string.h>
 using namespace std;
 
-int main(){
-string user_Name;
-string password;
-char choice;
-char L,S;
-cout<<"what do you want Login Or SignUp"<<endl;
-cout<<"please enter L for login and S for signUp : ";
-cin>>choice;
+void Login();
+void signUp();
 
-if(choice == 'L')
-cout<<"wellcome to the login page";
-else if(choice == 'S')
-cout<<"wellcome to the SignUp page";
-else
-cout<<"Please Enter a valid character  ";
+int main()
+{
+    string user_Name;
+    string password;
+    char choice;
+    char L, S;
+    cout << "what do you want Login Or SignUp" << endl;
+    cout << "please enter L for login and S for signUp : ";
+    cin >> choice;
 
+    if (choice == 'L'){ 
+        cout<<"wellcome to the login page "<<endl;  
 
+      Login();
+        
 
+    }
+    else if (choice == 'S'){
+        cout << "wellcome to the SignUp page";
 
-return 0;
+  
+     }else
+        cout << "Please Enter a valid character  ";
+    return 0;
 }
+
+void Login(){
+    string user_Name;
+    string password;
+    cout<<"Enter your user Name : ";
+    cin>>user_Name;
+    cout<<"Enter your password : ";
+    cin>>password;
+
+    cout<<endl<<endl<<"Logged in successfully : "<<endl;
+
+}
+
+
