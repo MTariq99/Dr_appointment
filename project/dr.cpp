2687799a748af9e2a34d00fbf5b8a112fb41dd3f
MTariq99 <you@example.com>
2022-05-17 11:22:55 +0500

    existingslots viewpatients

--- project/dr.cpp
+++ project/dr.cpp
@@ -1,108 +1,39 @@
-#include <iostream>
-#include <fstream>
-#include <string.h>
+#include<iostream>
+#include<fstream>
+#include<string.h>
 using namespace std;
 
-void LogIn();
-void signUp();
-int bookAnAppointment();
-int existingAppointment();
-void viewPatients();
+ int bookAnAppointment();
 
-int main()
-{
+int main(){
     string user_Name;
     string password;
-    int choice;
-    cout << endl  << "               Doctor appointment application                 " << endl;
+    string choice;
+    cout<< endl  << "               Doctor appointment application                 " << endl;
     cout << "--------------------------------------------------------------------" << endl<< endl;
     cout << "  1 : signUp " << endl;
     cout << "  2 : Login " << endl;
-    cout << "\nEnter a number accordingly : ";
-    cin >> choice;
+    cout<<"please enter login or signup : ";
+    cin>>choice;
 
-    switch (choice)
-    {
-    case 1:
-        signUp();
-        break;
-    case 2:
-        LogIn();
-        break;
-    case 3:
-        bookAnAppointment();
-        break;
-    case 4:
-        existingAppointment();
-        break;
-        cout << "wellcome to the SignUp page" << endl
-             << endl;
-        signUp();
-        break;
-    }
-
-    return 0;
-}
-
-void LogIn()
-{
-    string user_Name,password,user,pass,un,pw,userchoice;
+    if(choice == "login"){
+    string user_Name;
+    string password;
+    string userChoice;
     cout << "Enter your user Name : ";
     cin >> user_Name;
     cout << "Enter your password : ";
     cin >> password;
-    ifstream read("patientData.txt", ios::app);
-    getline(read,un);
-    getline(read,pw);
-    if(user_Name=="doctor" && password == "doctor"){
-         viewPatients();  
-}else if(un ==user_Name)
-    if(un == user_Name && pw == password){
-        cout<<user_Name<<" loggedIn successfully"<<endl;
-        cout<<"what do you want "<<endl;
-        cout<<"appointment"<<endl;
-        cout<<"want to see available slots"<<endl;
-        cout<<"3 for booking an appointment and 4 for existingslots:  ";
-        cin>>userchoice;
-        if(userchoice=="3")
-         bookAnAppointment();
-         else if(userchoice == "4")
-         existingAppointment();
-    }   else
-      {  cout << "please create your account "<<endl;
-        signUp();}
-    }
- 
-
-
-void signUp()
-{
-    string name;
-    string user_Name;
-    string password;
-    int phoneNumber;
-    cout<<"welcome to the signup page "<<endl;
-    cout << "Enter a user name  : ";
-    cin >> user_Name;
-    cout << "Enter a password : ";
-    cin >> password;
-    ofstream file;
-    file.open("patientData.txt", ios :: app);
-    file<<user_Name<<endl<<password<<endl;
-    file;
-    file.close();
-
-        cout<<user_Name<<endl;
-        cout<<password<<endl;
-    cout << endl<< endl<< "congratulations account created successfully " << endl;
-
-
-}
-
-int bookAnAppointment()
-{
-
-    system("cls");
+    if (user_Name == "admin" && password == "12345")
+        cout << endl
+             << "Admin Logged in successfully  " << endl;
+    else if (user_Name == "patient" && password == "123")
+{       cout << "Patient logged in successfully"<<endl;
+        cout<<"do you  want to book an appointment"<<endl;
+        cout<<" want to see existing slots "<<endl;
+        cin>>userChoice;
+        if(userChoice== "appointment"){
+                system("cls");
 
     cout << "\n ----- Book your appointment for particular time slot ---- \n";
     cout << "\n ----- Available slots are---- \n";
@@ -213,22 +144,17 @@ int bookAnAppointment()
     {
         out << choice << ":" << name.c_str() << "\n";
         out.close();
-        cout << "\n Appointment is Successfully Booked for Hours : " << (choice - 65) + 9 << " !!";
+        cout<< "\n Appointment is Successfully Booked for Hours : " << (choice - 65) + 9 << " !!";
     }
     else
     {
-        cout << "\n Error while saving booking";
+        cout<< "\n Error while saving booking";
     }
 
-    cout << "\n Please any key to continue..";
+    cout<< "\n Please any key to continue..";
     getchar();
     getchar();
-
-    return 0;
-}
-
-int existingAppointment()
-{
+  } else if(userChoice=="existingslots"){
     system("cls");
 
     cout << "\n ----- List of Available Appointments ---- \n";
@@ -303,24 +229,146 @@ int existingAppointment()
     cout << "\n Please any key to continue..";
     getchar();
     getchar();
-    return 0;
+
+  }
+
+
+    }
+
+
+}    else if (user_Name == "doctor" && password == "4321"){
+        cout << "doctor logged in successfully";
+
+}
+return 0;
 }
 
-void viewPatients(){
-    string drChoice;
-    cout<<"do you want to see patients appointments : ";
-    cin>>drChoice;
-    // if(drChoice == "yes")
-    // 'appointment.dat';
-    string viewpatient;
-    ifstream openfile ("appointment.dat");
-    if(openfile.is_open())
+
+int bookAnAppointment(){
+
+
+    system("cls");
+
+    cout << "\n ----- Book your appointment for particular time slot ---- \n";
+    cout << "\n ----- Available slots are---- \n";
+
+    ifstream read;
+    read.open("appointment.dat");
+
+    int hoursbook = 8;
+
+    int arr[13] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int recordFound = 0;
+
+    if (read)
     {
-        while(! openfile.eof())
+        string line;
+        char key = 'A';
+        int i = 9;
+
+        while (getline(read, line))
         {
-            getline(openfile, viewpatient);
-            cout << viewpatient << endl;
+            char temp = line[0];
+            int index = (temp);
+            arr[index] = 1;
+            recordFound = 1;
+        }
+        if (recordFound == 1)
+        {
+            cout << "\n Appointment List by Hours:";
+            char key = 'A';
+            int hours = 9;
+            for (int i = 0; i <= 12; i++)
+            {
+                if (i == 0)
+                {
+                    if (arr[i] == 0)
+                        cout << "\n " << key << "-> 0" << hours << " - Available";
+                    else
+                        cout << "\n " << key << "-> 0" << hours << " - Booked";
+                }
+
+                else
+                {
+                    if (arr[i] == 0)
+                        cout << "\n " << key << "->" << hours << " - Available";
+                    else
+                        cout << "\n " << key << "->" << hours << " - Booked";
+                }
+                hours++;
+                key++;
+            }
+        }
+
+        read.close();
+    }
+
+    if (recordFound == 0)
+    {
+        cout << "\n Appointment Available for following hours :";
+        char key = 'A';
+        for (int i = 9; i <= 21; i++)
+        {
+            if (i == 9)
+                cout << "\n " << key << " -> 0" << i << " - Available";
+            else
+                cout << "\n " << key << " -> " << i << " - Available";
+            key++;
         }
     }
-}
 
+    char choice;
+    cout<<"\n\n Choose your slot: ";
+    cin >> choice;
+
+    if (!(choice >= 'A' && choice <= 'Z'))
+    {
+        cout << "\n Error : Invalid Selection";
+        cout << "\n Please selction correct value from menu A- Z";
+        cout << "\n Press any key to continue";
+        getchar();
+        getchar();
+        system("cls");
+        bookAnAppointment();
+    }
+
+    int index = (choice - 65);
+    int isBooked = 1;
+    if (arr[index] == 0)
+        isBooked = 0;
+
+    if (isBooked == 1)
+    {
+        cout << "\n Error : This Appointment  slot is already booked";
+        cout << "\n Please select different slot time !!";
+        cout << "\n Press any key to continue!!";
+        getchar();
+        getchar();
+        system("cls");
+        bookAnAppointment();
+    }
+
+    string name;
+    cout << "\n Enter your Name:";
+    cin >> name;
+
+    ofstream out;
+    out.open("appointment.dat", ios::app);
+
+    if (out)
+    {
+        out << choice << ":" << name.c_str() << "\n";
+        out.close();
+        cout << "\n Appointment is Successfully Booked for Hours : " << (choice - 65) + 9 << " !!";
+    }
+    else
+    {
+        cout << "\n Error while saving booking";
+    }
+
+    cout << "\n Please any key to continue..";
+    getchar();
+    getchar();
+
+    return 0;
+}
